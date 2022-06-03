abc3bfedb3b9f7d1067b13f238d350345d539c1e
yajvendra <yajvendra.payal@asurion.com>
2022-06-03 00:09:02 -0500

    latest changes

--- dbtBasic/models/example/my_first_dbt_model.sql
+++ dbtBasic/models/example/my_first_dbt_model.sql
@@ -7,12 +7,13 @@
     Try changing "table" to "view" below
 */
 
+{{ config(materialized='table', alias ='first_model') }}
 
 with source_data as (
 
     select 1 as id
---    union all
---    select null as id
+    union all
+    select null as id
 
 )
 
@@ -20,8 +21,10 @@ select *
 from source_data
 where id is not null
 
+
 /*
     Uncomment the line below to remove records with null `id` values
 */
+
 
 
